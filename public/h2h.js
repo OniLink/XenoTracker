ae9ebe38b894eddff14bfc76723722a65fa0714d
Violet Kurtz <kody.j.kurtz@gmail.com>
2020-05-14 23:48:33 -0700

    Fully reworked into Express Handlebars, added sorting of H2Hs

--- public/h2h.js
+++ public/h2h.js
@@ -1,196 +1,50 @@
-// Mapping arrays
-const CHARS = [
-	"N/A",
-	"Shulk",  // 1
-	"Reyn",   // 2
-	"Fiora",  // 3
-	"Dunban", // 4
-	"Sharla", // 5
-	"Riki",   // 6
-	"Melia",  // 7
-	"Machina Fiora"
-];
+var h2h_rows = document.getElementsByClassName( "h2h-row" );
 
-const MAPS = [
-	"N/A",
-	"",                 // 1
-	"Colony 9",         // 2
-	"Tephra Cave",      // 3
-	"Bionis' Leg",      // 4
-	"Colony 6",         // 5
-	"Ether Mine",       // 6
-	"Satorl Marsh",     // 7
-	"Makna Forest",     // 8
-	"Frontier Village", // 9
-	"",                 // 10
-	"High Entia Tomb",  // 11
-	"Eryth Sea",        // 12
-	"Alcamoth",         // 13
-	"",                 // 14
-	"Prison Island",    // 15
-	"Valak Mountain",   // 16
-	"",                 // 17
-	"",                 // 18
-	"Fallen Arm",       // 19
-	"",                 // 20
-	"",                 // 21
-	"",                 // 22
-	"",                 // 23
-	"",                 // 24
-	"Bionis' Interior"  // 25
-];
-
-const AFFINITY = [
-	"Yellow",
-	"Green",
-	"Blue",
-	"White",
-	"Pink"
-];
-
-// Easy-access globals
-var h2h_db = null
-var h2h_table = null
-
-function loadH2HDB( filename ) {
-	// Open the JSON file with an HTTP request
-	var http = new XMLHttpRequest();
-	http.open( "GET", filename );
-	http.overrideMimeType( "application/json" );
-	http.onload = function() {
-		h2h_db = JSON.parse( http.responseText );
-
-		// Parse the DB
-		parseH2HDB( h2h_db );
-	};
-	http.send( null );
-}
-
-function parseH2HDB( h2hs ) {
-	var h2h_section = document.getElementById( "h2h-table" );
-	h2h_table = document.createElement( "table" );
-	h2h_section.appendChild( h2h_table );
-
-	parseH2HDBCreateTableHeader( h2h_table );
-	parseH2HDBCreateTableBody( h2h_table, h2hs );
-}
-
-function parseH2HDBCreateTableHeader( table ) {
-	var table_head = document.createElement( "thead" );
-	table.appendChild( table_head );
-	var table_head_row = document.createElement( "tr" );
-	table_head.appendChild( table_head_row );
-
-	// H2H Number
-	var new_element = document.createElement( "th" );
-	new_element.textContent = "#";
-	table_head_row.appendChild( new_element );
-
-	// H2H Name
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Title";
-	table_head_row.appendChild( new_element );
-
-	// First Character
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Character 1";
-	table_head_row.appendChild( new_element );
-	
-	// Second Character
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Character 2";
-	table_head_row.appendChild( new_element );
-	
-	// Affinity
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Affinity";
-	table_head_row.appendChild( new_element );
-	
-	/// @todo Time
-	
-	// Story Beat
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Story Chapter";
-	table_head_row.appendChild( new_element );
-
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Story Progress";
-	table_head_row.appendChild( new_element );
-
-	// Map
-	new_element = document.createElement( "th" );
-	new_element.textContent = "Map";
-	table_head_row.appendChild( new_element );
-}
-
-function parseH2HDBCreateTableBody( table, h2hs ) {
-	var table_body = document.createElement( "tbody" );
-	table.appendChild( table_body );
-
-	for( index in h2hs ) {
-		var new_row = document.createElement( "tr" );
-		table_body.appendChild( new_row );
-
-		// H2H Number
-		var new_element = document.createElement( "td" );
-		new_element.className += " right";
-		new_element.textContent = h2hs[ index ].sort_id; // Use the Sort ID, we gotta sort at the end
-		new_row.appendChild( new_element );
-
-		// Title
-		new_element = document.createElement( "td" );
-		new_element.textContent = h2hs[ index ].title;
-		new_row.appendChild( new_element );
-
-		// First Character
-		new_element = document.createElement( "td" );
-		new_element.textContent = CHARS[ parseInt( h2hs[ index ].first_char_id ) ];
-		new_row.appendChild( new_element );
-		
-		// Second Character
-		new_element = document.createElement( "td" );
-		new_element.textContent = CHARS[ parseInt( h2hs[ index ].second_char_id ) ];
-		new_row.appendChild( new_element );
-		
-		// Affinity
-		new_element = document.createElement( "td" );
-		var affinity = Math.floor( parseInt( h2hs[ index ].affinity ) / 1000.0 );
-		if( affinity >= 4 ) {
-			affinity = 4;
-		}
-		new_element.textContent = AFFINITY[ affinity ];
-		new_row.appendChild( new_element );
-		
-		/// @todo Time
-		
-		// Story Beat
-		new_element = document.createElement( "td" );
-		if( h2hs[ index ].story_flag > 0 ) {
-			new_element.textContent = h2hs[ index ].story_ch;
-		} else {
-			new_element.textContent = "N/A";
-		}
-		new_row.appendChild( new_element );
-
-		new_element = document.createElement( "td" );
-		if( h2hs[ index ].story_flag > 0 ) {
-			new_element.textContent = h2hs[ index ].story_beat;
-		} else {
-			new_element.textContent = "N/A";
-		}
-		new_row.appendChild( new_element );
-		
-		// Map
-		new_element = document.createElement( "td" );
-		new_element.textContent = MAPS[ parseInt( h2hs[ index ].map_id ) ];
-		new_row.appendChild( new_element );
-
-		// Add spoiler tag if necessary
-		if( parseInt( h2hs[ index ].first_char_id ) == 8 || parseInt( h2hs[ index ].second_char_id ) == 8 ) {
-			new_row.className += " spoiler";
-		}
+function onH2HMarkerChange( checkbox ) {
+	var row = checkbox.parentElement.parentElement;
+	if( checkbox.checked ) {
+		row.classList.add( "row-complete" );
+	} else {
+		row.classList.remove( "row-complete" );
 	}
 }
 
-// Load the quest database
-loadH2HDB( "h2h.json" );
+function sortTable( table_rows, sort_func ) {
+	// Bubble Sort!
+	var swapped;
+	do {
+		swapped = false;
+		for( var index = 0; index < table_rows.length - 1; ++index ) {
+			if( !sort_func( table_rows[ index ], table_rows[ index + 1 ] ) ) {
+				// Swap unsorted elements
+				var row1 = table_rows[ index ];
+				var row2 = table_rows[ index + 1 ];
+				row1.parentElement.insertBefore( row2, row1 );
+				swapped = true;
+			}
+		}
+	} while( swapped );
+}
+
+// Initial sort
+sortTable( h2h_rows, function( row1, row2 ) {
+	var cut_size = "h2h-".length;
+	var num_1 = parseInt( row1.id.slice( cut_size ) );
+	var num_2 = parseInt( row2.id.slice( cut_size ) );
+	return num_1 < num_2;
+} );
+
+// Add spoiler tags
+function hasSpoiler( row ) {
+	const INDEX_CHAR1 = 3;
+	const INDEX_CHAR2 = 4;
+	const SPOILER_NAME = "Machina Fiora";
+	var elements = row.getElementsByTagName( "td" );
+	return elements[ INDEX_CHAR1 ].textContent === SPOILER_NAME || elements[ INDEX_CHAR2 ].textContent === SPOILER_NAME;
+}
+
+for( index in h2h_rows ) {
+	if( hasSpoiler( h2h_rows[ index ] ) ) {
+		h2h_rows[ index ].classList.add( "spoiler" );
+	}
+}
